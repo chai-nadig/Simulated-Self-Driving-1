795f8a9d153fb929ccf52c3544dc12d160de97b8
chai nadig <chaitanya.nadig@gmail.com>
2018-08-06 07:26:32 -0700

    updated model.py with comments

--- model.py
+++ model.py
@@ -1,13 +1,21 @@
 import csv
 import cv2
+
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+
 import numpy as np
+
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-
+# The dataset to train
 folder = './train4'
 
 
+# This helper function reads all the lines in `driving_log.csv`
+# and returns them as an array.
 def read_dataset():
     lines = []
 
@@ -19,6 +27,19 @@ def read_dataset():
     return lines
 
 
+# A generator is used to read images in batches.
+# The default `batch_size` is 32.
+# For every line in `driving_log.csv`, this generator
+# produces 6 images.
+#
+# The first three images are from the center, left and right cameras.
+# These three images are then flipped to create augmented images
+# giving totally six images.
+#
+# The measurements for the left and right images are also corrected
+# using `correction`.
+#
+# The final batch is shuffled and returned
 def generator(samples, batch_size=32, correction=0.3):
     num_samples = len(samples)
     while 1:
@@ -52,27 +73,31 @@ def generator(samples, batch_size=32, correction=0.3):
             yield shuffle(X_train, y_train)
 
 
+# Read all lines and do a 20% split for training and validation
 all_samples = read_dataset()
 train_samples, validation_samples = train_test_split(all_samples, test_size=0.2)
-
 
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Lambda, Cropping2D
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-
+# Here we build the neural network
 model = Sequential()
+
+# Normalize the pixels and mean center them
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+
+# Crop the frames to ignore irrelevant pixel
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+
+# The following lines build a neural network as proposed by Nvidia
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
+model.add(Dense(1164))
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
@@ -80,7 +105,7 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
-                   validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+                    validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('model.h5')
 exit()
