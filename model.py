24093bd7278cd70b162cb3348b56421e6584d3ee
chai nadig <chaitanya.nadig@gmail.com>
2018-08-06 02:20:34 -0700

    use generator

--- model.py
+++ model.py
@@ -1,56 +1,66 @@
 import csv
 import cv2
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 images = []
 measurements = []
 
+folder = './train4'
 
-def read_dataset(folder, correction=0.3):
+
+def read_dataset():
     lines = []
-    images = []
-    measurements = []
 
     with open('{}/driving_log.csv'.format(folder)) as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
             lines.append(line)
 
-    for line in lines:
-        for i in range(3):
-            source_path = line[i]
-            filename = source_path.split('/')[-1]
-            current_path = '{}/IMG/{}'.format(folder, filename)
-            image = cv2.imread(current_path)
-            images.append(image)
-            measurement = float(line[3]) + (0 if i == 0 else correction if i == 1 else -correction)
-            measurements.append(measurement)
-
-    return images, measurements
+    return lines
 
 
-# images_train2, measurements_train2 = read_dataset('./train2')
-# images.extend(images_train2)
-# measurements.extend(measurements_train2)
+def generator(samples, batch_size=32, correction=0.3):
+    num_samples = len(samples)
+    while 1:
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset + batch_size]
 
-# images_train3, measurements_train3 = read_dataset('./train3')
-# images.extend(images_train3)
-# measurements.extend(measurements_train3)
+            batch_images = []
+            batch_measurements = []
+            for line in batch_samples:
+                for i in range(3):
+                    source_path = line[i]
+                    filename = source_path.split('/')[-1]
+                    current_path = '{}/IMG/{}'.format(folder, filename)
+                    image = cv2.imread(current_path)
+                    batch_images.append(image)
+                    measurement = float(line[3]) + (0 if i == 0 else correction if i == 1 else -correction)
+                    batch_measurements.append(measurement)
 
-images_train4, measurements_train4 = read_dataset('./train4')
-images.extend(images_train4)
-measurements.extend(measurements_train4)
+            augmented_images = []
+            augmented_measurements = []
+            for image, measurement in zip(batch_images, batch_measurements):
+                augmented_images.append(image)
+                augmented_measurements.append(measurement)
+                augmented_images.append(cv2.flip(image, 1))
+                augmented_measurements.append(measurement * -1.0)
 
-augmented_images = []
-augmented_measurements = []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image, 1))
-    augmented_measurements.append(measurement * -1.0)
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_measurements)
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+            yield shuffle(X_train, y_train)
+
+
+all_samples = read_dataset()
+train_samples, validation_samples = train_test_split(all_samples, test_size=0.2)
+
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Lambda, Cropping2D
@@ -71,7 +81,8 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
+                   validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('model.h5')
 exit()
